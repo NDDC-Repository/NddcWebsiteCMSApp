ba57a243d0bd15316ddcf9d7988297639793d18d
Orchiid <joannatolofari@gmail.com>
2023-11-08 13:19:47 +0100

    Latest Updates

---
+++ NddcWebsiteCMS/Pages/Home/PhotoSpeak/AllPhotos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NddcWebsiteCMS.Pages.Home.PhotoSpeak
+{
+    public class AllPhotosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
