7c8285f18635c7ef70e52b118f0f3d72a6797d9c
Orchiid <joannatolofari@gmail.com>
2023-11-13 13:01:27 +0100

    New Commit

---
+++ NddcWebsiteCMS/Pages/Home/Publications/AddPublications.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NddcWebsiteCMS.Pages.Home.Publications
+{
+    public class AddPublicationsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ NddcWebsiteCMS/Pages/Home/Testimonials/EditTestimonial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NddcWebsiteCMS.Pages.Home.Testimonial
+{
+    public class Edit_TestimonialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
