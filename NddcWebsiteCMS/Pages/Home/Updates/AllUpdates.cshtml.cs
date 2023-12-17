1358211c54ce1dab8878227a9813bd9f3733bda1
Dash Dogg <advantellocloud@gmail.com>
2023-12-17 17:20:30 +0100

    update

--- NddcWebsiteCMS/Pages/Home/Updates/AllUpdates.cshtml.cs
+++ NddcWebsiteCMS/Pages/Home/Updates/AllUpdates.cshtml.cs
@@ -1,12 +1,24 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebsiteCMSLibrary.Data.HomePage.Updates;
+using WebsiteCMSLibrary.Model.HomePage;
 
 namespace NddcWebsiteCMS.Pages.Home.Updates
 {
     public class AllUpdatesModel : PageModel
     {
+        private readonly IUpdatesData updateDb;
+        public List<MyUpdateModel> Updates { get; set; }
+        public readonly string _container;
+
+        public AllUpdatesModel(IUpdatesData updateDb, IConfiguration config)
+        {
+            this.updateDb = updateDb;
+            _container = config.GetConnectionString("AWSContainerUrl");
+        }
         public void OnGet()
         {
+            Updates = updateDb.AllUpdates();
         }
     }
 }
