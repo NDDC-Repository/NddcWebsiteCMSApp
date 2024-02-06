10c1df1163d1389f7788b7ab9020a020f783d95b
Orchiid <joannatolofari@gmail.com>
2024-02-06 14:58:37 +0100

    commit

---
+++ NddcWebsiteCMS/Pages/Home/Adverts/AddTenders.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NddcWebsiteCMS.Pages.Home.Adverts
+{
+    public class AddTendersModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
