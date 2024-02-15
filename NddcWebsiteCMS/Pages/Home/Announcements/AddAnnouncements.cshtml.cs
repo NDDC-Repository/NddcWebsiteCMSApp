c534b619930172c1fb174e1dd87a56dbc53dadad
Orchiid <joannatolofari@gmail.com>
2024-02-15 11:41:15 +0100

    new

---
+++ NddcWebsiteCMS/Pages/Home/Announcements/AddAnnouncements.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NddcWebsiteCMS.Pages.Home.Announcements
+{
+    public class AddAnnouncementsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
