adcdebaaa4a81fa4482b360e35f7262e44d94508
Dash Dogg <advantellocloud@gmail.com>
2024-02-16 13:27:48 +0100

    update

--- NddcWebsiteCMS/Pages/Media/Videos/DeleteVideos.cshtml.cs
+++ NddcWebsiteCMS/Pages/Media/Videos/DeleteVideos.cshtml.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebsiteCMSLibrary.Data.HomePage.Videos;
 
 namespace NddcWebsiteCMS.Pages.Media.Videos
 {
     public class DeleteVideosModel : PageModel
     {
+        private readonly IVideoData videoDb;
+
+        public DeleteVideosModel(IVideoData videoDb)
+        {
+            this.videoDb = videoDb;
+        }
         public void OnGet()
         {
+        }
+        public IActionResult OnPost(int? Id)
+        {
+            videoDb.DeleteVideo(Id.Value);
+
+            return RedirectToPage("AllVideos");
         }
     }
 }
