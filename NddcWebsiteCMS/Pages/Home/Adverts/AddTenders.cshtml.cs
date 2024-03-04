cb1342c2e21ac78415e86273699dac3c6e0fdc81
Orchiid <joannatolofari@gmail.com>
2024-03-04 16:52:55 +0100

    Added Code for Tenders and Publications

--- NddcWebsiteCMS/Pages/Home/Adverts/AddTenders.cshtml.cs
+++ NddcWebsiteCMS/Pages/Home/Adverts/AddTenders.cshtml.cs
@@ -1,26 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using WebsiteCMSLibrary.Data.HomePage.Tenders;
+using WebsiteCMSLibrary.Model.AzureStorage;
 using WebsiteCMSLibrary.Model.HomePage;
+using WebsiteCMSLibrary.Services.AzureStorageService;
 
 namespace NddcWebsiteCMS.Pages.Home.Adverts
 {
     public class AddTendersModel : PageModel
     {
         private readonly ITendersData tenderDb;
+        private readonly IAzureStorage storage;
+
         [BindProperty]
         public MyTenderModel Tender { get; set; }
-        public AddTendersModel(ITendersData tenderDb)
+
+        [BindProperty(SupportsGet = true)]
+        public IFormFile Upload { get; set; }
+        public AddTendersModel(ITendersData tenderDb, IAzureStorage storage)
         {
             this.tenderDb = tenderDb;
+            this.storage = storage;
         }
         public void OnGet()
         {
         }
-        public IActionResult OnPost()
+        public async Task<IActionResult> OnPost()
         {
-            tenderDb.AddTender(Tender);
+            string fileName = "Tenders/" + Path.ChangeExtension(Path.GetRandomFileName(), Path.GetExtension(Upload.FileName));
+            MyBlobResponseModel? response = await storage.UploadAsync(Upload, fileName);
+            Tender.DocumentUrl = response.Blob.Uri;
 
+            tenderDb.AddTender(Tender);
             return RedirectToPage("Tenders");
         }
     }
